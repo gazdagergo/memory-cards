4f87e40ca95bc93fde54bc5169a4a7498c62a28d
Gergely Gazda <info@gazdagergo.com>
2022-06-03 14:07:03 +0200

    feat(cards): introduce animation

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,58 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styled from '@emotion/styled'
 
-import Link from 'next/link';
-import { Button } from '@mantine/core';
 import {
   AppShell,
   Header,
-  Badge,
   Container,
   Card,
   Group,
   useMantineTheme,
-  Text,
-  ActionIcon,
 } from '@mantine/core';
-import { Bus } from 'tabler-icons-react';
 
+const FlipCard = styled(Group)`
+  // https://www.w3schools.com/howto/howto_css_flip_card.asp
 
+  background-color: transparent;
+  width: 160px;
+  height: 160px;
+  perspective: 1000px;
+
+  .flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  }
+
+  ${({ visible }) => visible ? `
+    .flip-card-inner {
+      transform: rotateY(180deg);
+    }` : ``}
+
+  .flip-card-front, .flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+  }
+
+  .flip-card-front {
+    background-color: #bbb;
+    color: black;
+  }
+
+  .flip-card-back {
+    background-color: #2980b9;
+    color: white;
+    transform: rotateY(180deg);
+  }
+`
 
 export default function Home() {
   const theme = useMantineTheme();
@@ -37,34 +72,21 @@ export default function Home() {
     >
       <Container size={1100}>
         <Group>
-        <Card radius="xl" shadow="sm" p="lg">
-          <Group style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
-            <ActionIcon color="grape" size="lg" radius="lg" variant="filled">
-              <Bus />
-            </ActionIcon>          
-            <Text weight={500}>Közlekedés</Text>
-          </Group>
-          <Group direction="column">
-            <Text>utak</Text>
-            <Text>megállók</Text>
-            <Text>közösségi közlekedés</Text>
-            <Text>biciklik</Text>
-          </Group>
-        </Card>
-        <Card radius="xl" shadow="sm" p="lg">
-          <Group style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
-            <ActionIcon color="grape" size="lg" radius="lg" variant="filled">
-              <Bus />
-            </ActionIcon>          
-            <Text weight={500}>Közlekedés</Text>
-          </Group>
-          <Group direction="column">
-            <Text>utak</Text>
-            <Text>megállók</Text>
-            <Text>közösségi közlekedés</Text>
-            <Text>biciklik</Text>
-          </Group>
-        </Card>
+          {Array(8).fill('a').map((_, i) => (
+            <FlipCard visible={i}>
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                <Image
+                  src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                  width={160}
+                  height={160}
+                  alt="Norway"
+                />
+                </div>
+                <div class="flip-card-back" />
+              </div>
+            </FlipCard>            
+          ))}
         </Group>
       </Container>
     </AppShell>
