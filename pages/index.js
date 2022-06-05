025a011af641739fb0c2c5ae98a2f8df0db9f6ce
Gergely Gazda <info@gazdagergo.com>
2022-06-05 22:32:18 +0200

    feat(page): add restart button

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,42 @@
 import { nanoid } from 'nanoid/non-secure'
+import { useRouter } from 'next/router'
 import axios from 'axios';
 import getShuffledArray from '../functions/getShuffledArray'
 import GameBoard from '../components/GameBoard';
 import Layout from '../components/layout';
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
+import styled from '@emotion/styled';
+
+const Overlay = styled.div`
+  position: fixed;
+  background: #FFFFFF88;
+  width: 100%;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  button {
+    font-size: 28px;
+    padding: 12px 24px;
+    border-radius: 8px;
+    cursor: pointer;
+    background: #2980b9;
+    color: white;
+
+    &:hover {
+      background: #3388b9;
+    }
+  }
+`
 
 const Home = ({ cards: initialCards }) => {
   const catSound = useRef(null);
+  const [isReady, setIsready] = useState(false)
+  const router = useRouter()
 
   useEffect(() => {
     catSound.current = new Audio('/meow.mp3')
@@ -14,11 +44,17 @@ const Home = ({ cards: initialCards }) => {
 
   const handleReady = () => {
     catSound.current.play()
+    setIsready(true)
+  }
+
+  const handleRestartClick = () => {
+    router.reload(window.location.pathname)
   }
 
   return (
     <Layout>
       <GameBoard initialCards={initialCards} onReady={handleReady} />
+      {isReady && <Overlay><button onClick={handleRestartClick}>Újrakezdem</button></Overlay>}
     </Layout>
   )
 }
